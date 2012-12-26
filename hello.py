8ed56ea5cc52bd9dc7a0bc41dd43fe00ec604f16
Dan McNair <glucnac@gmail.com>
2012-12-26 17:16:51 -0600

    playing with forms, added search

--- hello.py
+++ hello.py
@@ -16,8 +16,12 @@ db = web.database(dbn='sqlite', db='dp.sqlite')
 
 class index:
     def GET(self):
-        return 'Hello, world!'
+        # TODO: something useful
+        # list of patients? recent journal entries?
+        raise web.seeother('/pt/1/')
 
+
+# a list of multiple patients ... generally relatives
 
 class family:
     def GET(self, id_as_string):
@@ -29,6 +33,13 @@ class family:
         pts = list(db.where('patient', resparty=patientid))
         return render.family(pts)
 
+
+# patient display
+
+search_form = web.form.Form(
+    web.form.Textbox('query'),
+    web.form.Button('submit', type='submit', html='Search')
+)
 
 class patient_bounce:
     def GET(self, x):
@@ -48,7 +59,19 @@ class patient:
             resparty = pt
         journal = list(db.where('journal', order='ts DESC', limit=10, patientid=pt.id))
 
-        return render.pt(pt, resparty, journal)
+        return render.pt(search_form(), pt, resparty, journal)
+
+    def POST(self, *args):
+        f = search_form()
+        f.validates()
+        #TODO: lazy, SQL injection vector
+        pts = list(db.select('patient', where='lastname like "%s%%"' % f.query.get_value()))
+        if len(pts) == 1:
+            raise web.seeother('/pt/%d/' % pts[0].id)
+        elif len(pts) == 0:
+            return 'no patient found'
+        else:
+            return render.family(pts)
 
 
 if __name__ == "__main__":
