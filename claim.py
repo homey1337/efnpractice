827ab158b8a1ebcd3a4c43e27be0a96fa691de31
Dan McNair <glucnac@gmail.com>
2013-01-12 14:18:05 -0600

    claim thoughts, and a new table for the claim form
    58 fields OMG but most of them come out of the database somewhere

---
+++ claim.py
@@ -0,0 +1,101 @@
+"""ADA Claim Form Fields
+
+all dates formatted '%m/%d/%Y'
+
+HEADER INFORMATION
+
+1. Type of Transaction
+   Statement of Actual Services
+   Request for Predetermination
+
+2. Predetermination/Preauthorization Number
+
+PRIMARY PAYER INFORMATION
+
+3. Name & Address
+
+OTHER COVERAGE
+
+4. Other Dental or Medical Coverage? (No, Yes)
+
+5. Subscriber Name
+6. Date of Birth
+7. Gender
+8. Subscriber ID
+9. Plan/Group Number
+10. Relationship (Self, Spouse, Dependent, Other)
+11. Other Carrier Name & Address
+
+PRIMARY SUBSCRIBER
+
+12. Name & Address
+13. Date of Birth
+14. Gender
+15. Subscriber ID
+16. Plan/Group Number
+17. Employer Name
+
+PATIENT INFORMATION
+
+18. Relationship (Self, Spouse, Dependent, Other)
+19. Student Status (Full/Part Time)
+20. Name & Address
+21. Date of Birth (MM/DD/CCYY)
+22. Gender
+23. Patient ID/Account # (assigned by dentist)
+
+RECORD OF SERVICES
+
+24. Procedure Date
+25. Area of Oral Cavity
+26. Tooth System
+27. Tooth Number(s)
+28. Tooth Surface(s)
+29. Procedure
+30. Description
+31. Fee
+32. Other Fee(s)
+33. Total Fee
+
+34. Place an X on each missing tooth
+35. Remarks
+
+AUTHORIZATIONS
+
+36. Patient/Guardian Signature
+37. Assignment of Benefits
+
+ANCILLARY INFORMATION
+
+38. Place of Treatment (Office, Hospital, ECF, Other)
+39. Number of enclosures
+40. Is Treatment for Orthodontics? (No, Yes)
+41. Date Appliance Placed
+42. Months of Treatment Remaining
+43. Replacement of Prosthesis? (No, Yes)
+44. Date Prior Placement
+45. Treatment Resulting From (Occupational illness, Auto accident, Other accident)
+46. Date of Accident
+47. Auto Accident State
+
+BILLING DENTIST OR ENTITY
+
+48. Name & Address
+49. NPI
+50. License Number
+51. SSN or TIN
+52. Phone Number
+52A. Additional Provider ID
+
+TREATING DENTIST
+
+53. Signature
+54.  NPI
+55. License Number
+56. Address
+56A. Treating Provider Specialty
+57. Phone Number
+58. Additional Provider ID
+
+Whew!
+"""
--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ create table if not exists patient
   name string,
   resparty integer references patient (id),
   birthday date,
+  gender string,
   notes text);
 
 create table if not exists journal
@@ -49,13 +50,15 @@ create table if not exists carrier
   eclaim string);
 
 create table if not exists plan
- (id integer primary key,
-  journalid integer references journal(id),
-  prim integer references plan(id),
+ (journalid integer primary key references journal(id),
+  secondaryto integer references plan(id),
   carrierid integer references carrier(id),
   insuredid integer references patient(id),
+  relationship string,
+  student_status string,
   groupnum string,
   idnum string,
+  employer string,
   deductible currency,
   maximum currency,
   prevent integer,
@@ -64,12 +67,96 @@ create table if not exists plan
   notes text);
 
 create table if not exists claim
- (id integer primary key,
-  planid integer references plan (id),
-  journalid integer references journal (id),
+ (journalid integer primary key references journal (id),
+  preauth bool,
+  planid integer references plan (journalid),
   filed datetime,
   closed datetime,
   notes text);
+
+create table if not exists claimform
+ (claimid integer primary key references claim(journalid),
+  -- from claimid -> preauth field
+  type_of_transaction string,
+  -- leave blank
+  predetermination_number string,
+  -- claimid -> planid -> carrierid -> name & address
+  primary_payer_name_address text,
+  -- if claimid -> planid -> secondary_to
+  -- or if there is a plan with secondary_to == claimid -> planid
+  -- set otherid to that plan ...
+  -- whether we found other coverage
+  other_coverage bool,
+  -- otherid -> insuredid -> name
+  other_subscriber_name string,
+  -- otherid -> insuredid -> birthday
+  other_subscriber_dob string,
+  -- otherid -> insuredid -> gender
+  other_subscriber_gender string,
+  -- otherid -> idnum
+  other_subscriber_id string,
+  -- otherid -> groupnum
+  other_subscriber_group string,
+  -- otherid -> relationship
+  relationship_to_other_subscriber string,
+  -- otherid -> carrierid -> name & address
+  other_carrier_name_address text,
+  -- claimid -> planid -> insuredid -> name
+  -- and lookup the most recent address in insuredid's journal
+  primary_subscriber_name_address text,
+  -- claimid -> planid -> insuredid -> birthday
+  primary_subscriber_dob string,
+  -- claimid -> planid -> insuredid -> gender
+  primary_subscriber_gender string,
+  -- claimid -> planid -> idnum
+  primary_subscriber_id string,
+  -- claimid -> planid -> groupnum
+  primary_subscriber_group string,
+  -- claimid -> planid -> employer
+  primary_subscriber_employer string,
+  -- claimid -> planid -> relationship
+  relationship_to_primary_subscriber string,
+  -- claimid -> planid -> student_status
+  patient_student_status string,
+  -- claimid -> journalid -> patientid -> name
+  -- and lookup the most recent address in patientid's journal
+  patient_name_address text,
+  -- claimid -> journalid -> patientid -> birthday
+  patient_dob string,
+  -- claimid -> journalid -> patientid -> gender
+  patient_gender string,
+  -- claimid -> journalid -> patientid
+  patient_id string,
+  -- always zero
+  other_fees currency default 0.00,
+  -- sensible defaults, should be a way to modify
+  missing_teeth string default '',
+  remarks string default '',
+  place_of_treatment string default 'office',
+  number_of_enclosures string default NULL,
+  orthodontic_treatment bool default false,
+  orthodontic_start_date string default '',
+  orthodontic_months_remaining string default '',
+  prosthesis_replacement bool default NULL,
+  prosthesis_original_date string default '',
+  treatment_resulting_from string default '',
+  date_of_accident string default '',
+  state_of_accident string default '',
+  -- office & provider information can be in config.py until further notice
+  -- office information
+  billing_name_address text,
+  billing_npi string,
+  billing_license string,
+  billing_tin string,
+  billing_phone string,
+  billing_additional_id string,
+  -- provider information
+  treating_npi string,
+  treating_license string,
+  treating_address string,
+  treating_specialty string,
+  treating_phone string,
+  treating_additional_id string);
 
 create table if not exists cdt
  (id integer primary key,
